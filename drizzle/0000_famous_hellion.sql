bfbb0b4b4eec627d5640b833ec1752f5f44e8ffa
hildanku <hildankutomo@gmail.com>
2025-04-09 13:08:31 +0700

    feat: init development setup for database

---
+++ drizzle/0000_famous_hellion.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `users` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`created_at` bigint unsigned NOT NULL,
+	`updated_at` bigint unsigned NOT NULL,
+	`username` char(100) NOT NULL,
+	`name` text NOT NULL,
+	`role` text NOT NULL DEFAULT ('operator'),
+	`avatar` text,
+	`email` text NOT NULL,
+	CONSTRAINT `users_id` PRIMARY KEY(`id`),
+	CONSTRAINT `users_username_unique` UNIQUE(`username`)
+);
---
+++ src/config/db/schema.ts
@@ -0,0 +1,20 @@
+import { bigint, char, mysqlTable, serial, text } from 'drizzle-orm/mysql-core'
+
+export const userTable = mysqlTable('users', {
+    id: serial().primaryKey(),
+    created_at: bigint({ mode: 'number', unsigned: true })
+        .notNull()
+        .$defaultFn(() => new Date().getTime()),
+    updated_at: bigint({ mode: 'number', unsigned: true })
+        .notNull()
+        .$defaultFn(() => new Date().getTime()),
+    username: char({ length: 100 }).notNull().unique(),
+    name: text().notNull(),
+    role: text({ enum: ['production_manager', 'operator'] })
+        .default('operator')
+        .notNull(),
+    avatar: text(),
+    email: text().notNull(),
+})
+
+export type User = typeof userTable.$inferSelect
