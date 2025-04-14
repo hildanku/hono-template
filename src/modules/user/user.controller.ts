351d1e2830d6f84136526e54e94428daf08d139c
hildanku <hildankutomo@gmail.com>
2025-04-15 01:40:50 +0700

    feat: add GET / to get list of users - add zod validator

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,1 +1,20 @@
+import { Hono } from 'hono'
+import { UserRepository } from './user.repository.js'
+import { appResponse } from '../../lib/response.js'
+import { FOUND, NOT_FOUND, SOMETHING_WHEN_WRONG } from '../../lib/constant.js'
 
+export const userController = new Hono()
+    .get('/', async (c) => {
+        const userRepo = new UserRepository()
+
+        try {
+            const users = await userRepo.list()
+            if (!users || users.length === 0) {
+                return appResponse(c, 404, `User ${NOT_FOUND}`, null)
+            }
+            return appResponse(c, 200, `User ${FOUND}`, users)
+        } catch (error) {
+            console.error(error)
+            return appResponse(c, 500, SOMETHING_WHEN_WRONG, null)
+        }
+    })
