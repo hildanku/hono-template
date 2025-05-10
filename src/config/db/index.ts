ed4f52f9072cdd613c667ad4030c2e406d2dacd0
hildanku <hildankutomo@gmail.com>
2025-05-10 10:21:55 +0700

    feat: add logging and implement in db connection

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -1,4 +1,12 @@
 import { drizzle } from 'drizzle-orm/mysql2'
 import 'dotenv/config'
+import mysql from 'mysql2/promise'
+import { drizzleLogger } from '../logging.js'
 
-export const db = drizzle(process.env.DATABASE_URL!)
+const connection = await mysql.createConnection({
+    uri: process.env.DATABASE_URL!
+})
+
+export const db = drizzle(connection, {
+    logger: drizzleLogger
+})
--- src/config/logging.ts
+++ src/config/logging.ts
@@ -1,8 +1,8 @@
 import * as winston from 'winston'
-import { Logger } from 'drizzle-orm'
+import type { Logger } from 'drizzle-orm'
 
 const { combine, timestamp, printf, colorize, align } = winston.format;
-
+// thanks syahrulbudif
 export const logger = winston.createLogger({
     level: 'info',
     format: combine(
@@ -18,6 +18,32 @@ export const logger = winston.createLogger({
 
 export const drizzleLogger: Logger = {
     logQuery(query: string, params: unknown[]) {
+        const formattedQuery = query.replace(/\s+/g, ' ').trim()
 
+        const formattedParams = params.map(param => {
+            if (typeof param === 'string') {
+                return param.replace(/^"(.*)"$/, '$1')
+            }
+            return param
+        })
+
+        const paramCount = (query.match(/\$\d+/g) || []).length
+
+        const groupedParams = []
+        for (let i = 0; i < formattedParams.length; i += paramCount) {
+            groupedParams.push(formattedParams.slice(i, i + paramCount))
+        }
+
+        const logMessage = `
+                =======================
+                Query: ${formattedQuery}
+                Parameters:
+                ${groupedParams
+                .map((params, index) => `  Row ${index + 1}: ${JSON.stringify(params)}`)
+                .join('\n')}
+                =======================
+        `
+
+        logger.info(logMessage)
     },
 }
